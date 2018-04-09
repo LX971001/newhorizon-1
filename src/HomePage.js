c49d3d6b94443a473c50f58f8426e93d59f1ea0e
EdwardZhang <super_mr.z@hotmail.comm>
2018-04-09 22:17:46 +0800

    LoginIssues

--- src/HomePage.js
+++ src/HomePage.js
@@ -5,17 +5,30 @@ import InforCarousel from './InforCarousel';
 import CardList from './CardList';
 import './HomePage.css';
 import { queryLessons } from './service';
+import axios from 'axios'
 
-const { Header, Content } = Layout;
+const { Header, Content, Sider } = Layout;
 
 class HomePage extends React.Component {
   state = {
     lessons:[],
-    title:'jincan'
+    title:'jincan',
+    loginstatus:false
   }
   componentDidMount() {
     var that = this;
     that.getJsonData();
+
+    // axios.get('/api/loginstatus')
+    // .then(function (response) {
+    //   this.setState({loginstatus: response.status})
+    //   // console.log(response);
+    // })
+    // .catch(function (error) {
+    //   console.log(error);
+    // });
+
+
   }
   getJsonData = () => {
     queryLessons().then(res => {
@@ -36,6 +49,10 @@ class HomePage extends React.Component {
             <Menu.Item key="1"><Link to='/home'>首页</Link></Menu.Item>
             <Menu.Item key="2"><Link to='showquestions'>问答</Link></Menu.Item>
             <Menu.Item key="3"><Link to='exam'>评测</Link></Menu.Item>
+            <Menu.Item key="4">
+            {this.state.loginstatus?<Link to='/admin'>管理后台</Link>:<Link to='/login'>登录</Link>}
+            {console.log(this.state.loginstatus)}
+            </Menu.Item>
           </Menu>
         </Header>
         <Content stylel={{width:"100%"}}>
